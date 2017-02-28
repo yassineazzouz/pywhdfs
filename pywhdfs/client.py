0076eda61acd291cedec3dfb59f04c4536d5eeea
Yassine <yassine.azzouz@gmail.com>
2017-02-28 11:56:53 +0100

    Added missing import

--- pywhdfs/client.py
+++ pywhdfs/client.py
@@ -7,6 +7,7 @@ from .utils.utils import *
 from contextlib import contextmanager
 from multiprocessing.pool import ThreadPool
 from threading import Lock, Semaphore
+from subprocess import call
 import requests as rq
 import logging as lg
 import itertools as it
